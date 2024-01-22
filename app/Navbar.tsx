c78cf287f7efedf7e9526c0bf310be9090d88106
kapilrc <kapilrc@gmail.com>
2024-01-22 15:02:45 +0530

    accessing session on client and server

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="mr-5">
@@ -10,9 +14,17 @@ const Navbar = () => {
       <Link href="/users" className="mr-5">
         Users
       </Link>
-      <Link href="/api/auth/signin" className="mr-5">
-        Login
-      </Link>
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'unauthenticated' && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
+      {status === 'authenticated' && (
+        <Link href="/" className="mr-5">
+          {session.user!.name}
+        </Link>
+      )}
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './Navbar';
+import AuthProvider from './auth/Provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +15,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" data-theme="winter">
       <body className={inter.className}>
-        <Navbar />
-        <main className="p-5">{children}</main>
+        <AuthProvider>
+          <Navbar />
+          <main className="p-5">{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
